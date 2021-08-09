5250d935fe27c840afd524f966bc0d2506aaf3a7
Nelcione Valério Dias <33238924+landernodias@users.noreply.github.com>
2021-08-09 20:57:31 -0300

    API-NodeJS-Express
    Servidor

--- src/servidor.js
+++ src/servidor.js
@@ -7,24 +7,20 @@ const bancoDeDados = require('./bancoDeDados')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//req: request res: response next: vai para a proxima chamada
-app.get('/produtos', (req, res, next) => { //função middleware
+app.get('/produtos', (req, res, next) => { 
     res.send(bancoDeDados.getProdutos()) //Converte um objeto em JSON
 })
 
-//pegando por parametro
 app.get('/produtos/:id', (req, res, next) => {
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-//salvando os dados 
-//obs: o post que vem de um formulario é pego dessa maneira apartir da requisição
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco
     })
-    res.send(produto) //Saida gerada é um JSON
+    res.send(produto) 
 })
 app.put('/produtos/:id', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
@@ -32,12 +28,12 @@ app.put('/produtos/:id', (req, res, next) => {
         nome: req.body.nome,
         preco: req.body.preco
     })
-    res.send(produto) //Saida gerada é um JSON
+    res.send(produto) 
 })
 
 app.delete('/produtos/:id', (req, res, next) => {
     const produto = bancoDeDados.deleteProduto(req.params.id)
-    res.send(produto) //Saida gerada é um JSON
+    res.send(produto) 
 })
 
 app.listen(porta, () => {
